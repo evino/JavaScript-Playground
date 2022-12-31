0cf5e3360fdee0a47e046c455c5689f10fc82666
Evin Odisho <evinodisho@gmail.com>
2022-12-30 18:25:26 -0800

    trying to get pouchdb setup

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log('Hello from JavaScript!');
+
+var PouchDB = require('pouchdb');
+var db = new PouchDB('EvinDB');
+
+var doc = {
+    _id : "evin",
+    name : 'Evin',
+    age : 21,
+    school : 'UCSC'
+};
+
+db.put(doc);
+
+
+
+db.get('evin').then(function (doc){
+    console.log(doc)
+});
